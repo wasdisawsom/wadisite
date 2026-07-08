d13698e91bec3baedecfa55eb4cf74b7671931de
wasdisawsom <wasdisawsom@gmail.com>
2026-07-08 12:37:57 +0800

    Navbar can now be imported/linked with one line in pages, made the lightmode of wawa slightly darker because OW MY EYES, updated wawa style to match navbar

---
+++ src/public/header/js/logix.js
@@ -0,0 +1,27 @@
+try {
+    document.addEventListener("DOMContentLoaded", async function() {
+        const headerElement = document.createElement("header");
+
+        headerElement.innerHTML = `
+        <li class="home"><a href="/"><img src="/images/wad.jpg" alt="" href="/"><p>Home</p></a></li>
+        <li class="apps-dropdown">
+            <p class="dropdownbutton">Apps</p>
+            <div class="dropdowncontents">
+                <a href="/wawa">wawa encoder</a>
+                <a href="#">placeholder lmao</a>
+            </div>
+        </li>
+        `;
+
+        const styleLink = document.createElement("link");
+
+        styleLink.rel = "stylesheet";
+        styleLink.href = "/header/css/header.css";
+
+        document.body.insertBefore(headerElement, document.body.children[0]);
+        document.head.appendChild(styleLink)
+
+    })
+} catch (error) {
+    console.error("Error occured somewhere, please yell at wadi on discord to fix it\nError: \n" + error);
+}
